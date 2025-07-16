8345c064c6e1f2f6849d3d906afac2ec24ab6bee
Eason Lee <demolure@outlook.com>
2025-07-17 00:14:22 +0800

    On branch master Your branch is up to date with 'origin/master'.
    Changes to be committed:
    	modified:   bubble_sort/main.cpp

--- bubble_sort/main.cpp
+++ bubble_sort/main.cpp
@@ -5,13 +5,23 @@ const int N = 10000;
 int n;
 int a[N];
 
+
+// 考研代码比竞赛多一个flag
 void bubble_sort()
 {
     for (int i = 0; i < n - 1; i++)
+    {
+        bool flag = 0; // 用于表示该轮是否有swap，没有说明有序，可以直接break
         for (int j = 0; j < n - i - 1; j++)
+        {
             if (a[j] > a[j + 1])
+            {
                 swap(a[j], a[j + 1]);
-
+                flag = 1;
+            }
+        }
+        if (!flag) return; // 节省无用功
+    }
 }
 
 int main()
