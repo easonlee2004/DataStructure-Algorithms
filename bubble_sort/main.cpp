8cb1ca29b5dce7f251b1ee69bec8f6005aa7dca5
Eason Lee <demolure@outlook.com>
2025-07-17 00:04:17 +0800

    On branch master Your branch is up to date with 'origin/master'.
    Changes to be committed:
    	modified:   bubble_sort/main.cpp

--- bubble_sort/main.cpp
+++ bubble_sort/main.cpp
@@ -8,17 +8,10 @@ int a[N];
 void bubble_sort()
 {
     for (int i = 0; i < n - 1; i++)
-    {
         for (int j = 0; j < n - i - 1; j++)
-        {
             if (a[j] > a[j + 1])
-            {
-                a[j] ^= a[j + 1];
-                a[j + 1] ^= a[j];
-                a[j] ^= a[j + 1];
-            }
-        }
-    }
+                swap(a[j], a[j + 1]);
+
 }
 
 int main()
